52e20d24031c7aef44d97a958cea30f702bc25f1
PrincessDie <iannzits@live.com>
2015-07-25 23:10:41 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1684,7 +1684,7 @@
             },
 
             clearchatCommand: {
-                command: ['bostejarchat', 'deletarchat']
+                command: ['bostejarchat', 'deletarchat'],
                 rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
